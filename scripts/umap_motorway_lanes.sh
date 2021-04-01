eb3c0fc34126e29c8f4433e7bdf624bf51ce3be8
Dongha Hwang <depth221@gmail.com>
2021-04-01 20:34:53 +0900

    add codes for trunk

---
+++ scripts/umap_motorway_lanes.sh
@@ -0,0 +1,11 @@
+echo "motorways without lanes"
+#[out:json][timeout:100];
+#{{geocodeArea:South Korea}}->.searchArea;
+#(
+#  way["highway"="motorway"][!"lanes"](area.searchArea);
+#);
+#out body;
+#>;
+#out skel qt;
+wget -O datasets/without_lanes/motorways_without_lanes.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A100%5D%3Barea%283600307756%29%2D%3E%2EsearchArea%3B%28way%5B%22highway%22%3D%22motorway%22%5D%5B%22lanes%22%21%7E%22%2E%2A%22%5D%28area%2EsearchArea%29%3B%29%3Bout%3B%3E%3Bout%20skel%20qt%3B%0A'
+sleep 300
