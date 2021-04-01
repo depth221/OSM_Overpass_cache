53b18d14ac60bb54e50578d98beec91385c5f252
Dongha Hwang <depth221@gmail.com>
2021-04-01 19:44:46 +0900

    update

--- umap_motorway_surface.sh
+++ umap_motorway_surface.sh
@@ -7,5 +7,5 @@ echo "motorways without surface"
 #out body;
 #>;
 #out skel qt;
-wget -O motorways_without_surface.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A100%5D%3Barea%283600307756%29%2D%3E%2EsearchArea%3B%28way%5B%22highway%22%3D%22motorway%22%5D%5B%22surface%22%21%7E%22%2E%2A%22%5D%28area%2EsearchArea%29%3B%29%3Bout%3B%3E%3Bout%20skel%20qt%3B%0A'
+wget -O datasets/without_surface/motorways_without_surface.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A100%5D%3Barea%283600307756%29%2D%3E%2EsearchArea%3B%28way%5B%22highway%22%3D%22motorway%22%5D%5B%22surface%22%21%7E%22%2E%2A%22%5D%28area%2EsearchArea%29%3B%29%3Bout%3B%3E%3Bout%20skel%20qt%3B%0A'
 sleep 300
