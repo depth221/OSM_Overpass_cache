6ade57b285815cfc561d9758894d5bbf638679de
Dongha Hwang <depth221@gmail.com>
2021-04-01 21:09:08 +0900

    Update umap_trunk_lanes.sh

--- scripts/umap_trunk_lanes.sh
+++ scripts/umap_trunk_lanes.sh
@@ -7,5 +7,5 @@ echo "trunks without lanes"
 #out body;
 #>;
 #out skel qt;
-wget -O datasets/without_lanes/trunk_without_lanes.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A100%5D%3Barea%283600307756%29%2D%3E%2EsearchArea%3B%28way%5B%22highway%22%3D%22trunk%22%5D%5B%22lanes%22%21%7E%22%2E%2A%22%5D%28area%2EsearchArea%29%3B%29%3Bout%3B%3E%3Bout%20skel%20qt%3B%0A'
+wget -O datasets/without_lanes/trunks_without_lanes.json 'https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A100%5D%3Barea%283600307756%29%2D%3E%2EsearchArea%3B%28way%5B%22highway%22%3D%22trunk%22%5D%5B%22lanes%22%21%7E%22%2E%2A%22%5D%28area%2EsearchArea%29%3B%29%3Bout%3B%3E%3Bout%20skel%20qt%3B%0A'
 sleep 300
