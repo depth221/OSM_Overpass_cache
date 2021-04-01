2d5c4b71fd83803d12f430217da927dea5e414b2
Dongha Hwang <depth221@gmail.com>
2021-04-01 19:01:55 +0900

    Update count.sh

--- count.sh
+++ count.sh
@@ -3,3 +3,5 @@ echo "objects from South Korea Open Data Portal" >> ./count.txt
 cat ./objects_from_ROK_OpenData.json | grep 'source' | wc -l >> ./count.txt
 echo "motorways without lanes" >> ./count.txt
 cat ./motorways_without_lanes.json | grep 'highway' | wc -l >> ./count.txt
+echo "trunks without lanes" >> ./count.txt
+cat ./trunks_without_lanes.json | grep 'highway' | wc -l >> ./count.txt
